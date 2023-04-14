f385897051d2b4b76b1aac869ec463a3c500a89d
ranaji038 <mailforranaji@gmail.com>
2023-04-15 02:37:40 +0530

    cors error Handling again

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "https://chat-app-kcpr.onrender.com",
+    origin: "https://serene-kelpie-02d2a4.netlify.app/",
     methods: ["GET", "POST"],
   },
 });
